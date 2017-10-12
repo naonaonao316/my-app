4e9a5289c738b718b10f9c3a66fc43e596c1d900
Nao Takegawa <nao.takegawa316@gmail.com>
2017-10-12 15:53:04 +0900

    initialize react-md

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import WebFontLoader from 'webfontloader';
 
+WebFontLoader.load({
+  google: {
+    families: ['Roboto:300,400,500,700', 'Material Icons'],
+  },
+});
+
+ ReactDOM.render(<App />, document.getElementById('root'));
+ registerServiceWorker();
+/*
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -144,3 +156,4 @@ function calculateWinner(squares) {
   }
   return null;
 }
+*/
